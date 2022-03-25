588130c63288b37dbf286268f32a145a66748aec
DanielLeivaCalderon <31292016+DanielLeivaCalderon@users.noreply.github.com>
2022-03-25 13:31:47 -0600

    Create test1.cs

---
+++ test1.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Diagnostics;
+using Xunit;
+
+namespace CrisXUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int val = 3;
+            Debug.Assert(val != 2, "Value should not be 2.");
+        
+        }
+    }
+}
